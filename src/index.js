4140cb964f492ff05ce4373ac2303474d7acf1b5
Tebsm <138114830+Tebsm@users.noreply.github.com>
2023-08-06 20:43:31 +0200

    HTML & JS functionality
    added day/time and weather icon to the submittedCityWeather function

--- src/index.js
+++ src/index.js
@@ -1,88 +1,3 @@
-function localCityWeather(position) {
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric&appid=cbc2508db8ce71927b5728c130e5af89`;
-
-  fetch(apiUrl)
-    .then((response) => response.json())
-    .then((data) => {
-      let cityName = data.name;
-      let temperature = Math.round(data.main.temp);
-      let weatherDescription = data.weather[0].description;
-      // console.log(weatherDescription);
-      let winSpeed = Math.round(data.wind.speed);
-      // console.log(data);
-      function localCityTemp(local) {
-        local.preventDefault();
-        let h2 = document.querySelector("h2");
-        h2.innerHTML = `Currently in ${cityName}`;
-
-        let temp = document.querySelector("#temp");
-        temp.innerHTML = `${temperature}`;
-        let description = document.querySelector("#weatherDescription");
-        description.innerHTML = `${weatherDescription}`;
-        description.textContent = weatherDescription;
-        let wind = document.querySelector("#win");
-        wind.innerHTML = `Wind:${winSpeed} m/s`;
-        let humidity = document.querySelector("#hum");
-        humidity.innerHTML = `Humidity:${data.main.humidity}%`;
-      }
-
-      let currentLocButton = document.querySelector("#currentLoc");
-      currentLocButton.addEventListener("click", localCityTemp);
-    });
-  navigator.geolocation.getCurrentPosition(localCityWeather);
-
-  function currentDayTime() {
-    let now = new Date();
-    let options = { weekday: "long" };
-    let day = now.toLocaleString("en-US", options);
-    let hours = now.getHours();
-    let minutes = now.getMinutes().toString().padStart(2, "0");
-
-    let currentTime = `${day} ${hours}:${minutes}`;
-
-    document.querySelector("#current-time").textContent = currentTime;
-  }
-}
-
-navigator.geolocation.getCurrentPosition(localCityWeather);
-
-function currentDayTime() {
-  let now = new Date();
-  let options = { weekday: "long" };
-  let day = now.toLocaleString("en-US", options);
-  let hours = now.getHours();
-  let minutes = now.getMinutes().toString().padStart(2, "0");
-
-  let currentTime = `${day} ${hours}:${minutes}`;
-
-  document.querySelector("#current-time").textContent = currentTime;
-}
-
-currentDayTime();
-function submittedCityWeather(city) {
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=cbc2508db8ce71927b5728c130e5af89&units=metric&dt=UTC`;
-  fetch(apiUrl)
-    .then((response) => response.json())
-    .then((data) => {
-      let temperature = Math.round(data.main.temp);
-      let windSpeed = Math.round(data.wind.speed);
-      let weatherDescription = data.weather[0].description;
-      let temp = document.querySelector("#temp");
-      temp.innerHTML = `${temperature}`;
-      let weatherDes = document.querySelector("#weatherDescription");
-      weatherDes.innerHTML = `${weatherDescription}`;
-
-      let wind = document.querySelector("#win");
-      wind.innerHTML = `Wind: ${windSpeed} m/s`;
-      let humidity = document.querySelector("#hum");
-      humidity.innerHTML = `Humidity: ${data.main.humidity}%`;
-    })
-    .catch((error) => {
-      console.log("Error:", error);
-      alert("An error occurred while fetching the weather data.");
-    });
-}
-
 function submitCity(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-text-input");
@@ -99,14 +14,42 @@ function submitCity(event) {
 let form = document.querySelector("#city-form");
 form.addEventListener("submit", submitCity);
 
-let tempElement = document.getElementById("temp");
-let celsiusButton = document.getElementById("celsius");
-let fahrenheitButton = document.getElementById("fahrenheit");
+function submittedCityWeather(city) {
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=cbc2508db8ce71927b5728c130e5af89&units=metric&dt=UTC`;
+  fetch(apiUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      let temperature = Math.round(data.main.temp);
+      let windSpeed = Math.round(data.wind.speed);
+      let weatherDescription = data.weather[0].description;
+      let weatherIcon = data.weather[0].icon;
+      let temp = document.querySelector("#temp");
+      temp.innerHTML = `${temperature}`;
+      let weatherDes = document.querySelector("#weatherDescription");
+      weatherDes.innerHTML = `${weatherDescription}`;
 
-fahrenheitButton.addEventListener("click", function () {
-  tempElement.textContent = "70";
-});
+      let iconUrl = `http://openweathermap.org/img/wn/${weatherIcon}.png`;
+      let weatherIconDiv = document.querySelector("#weatherIcon");
+      weatherIconDiv.innerHTML = `<img src="${iconUrl}" alt="Weather Icon">`;
+  
+      let wind = document.querySelector("#win");
+      wind.innerHTML = `Wind: ${windSpeed} m/s`;
+      let humidity = document.querySelector("#hum");
+      humidity.innerHTML = `Humidity: ${data.main.humidity}%`;
+let timezoneOffset = data.timezone; // Timezone offset in seconds
+    let now = new Date();
+    let utcTime = now.getTime() + now.getTimezoneOffset() * 60 * 1000; // Current UTC time
+    let cityTime = new Date(utcTime + timezoneOffset * 1000); // Current city time
 
-celsiusButton.addEventListener("click", function () {
-  tempElement.textContent = `21`;
-});
+    let options = { weekday: "long", hour: "2-digit", minute: "2-digit" };
+    let currentTime = cityTime.toLocaleString("en-US", options);
+
+    // Update the element with the ID "current-time"
+    document.querySelector("#current-time").textContent = currentTime;
+
+    })
+    .catch((error) => {
+      console.log("Error:", error);
+      alert("An error occurred while fetching the weather data.");
+    });
+}
